ec27bfb25c05c79d766ee8bb7e4875a73accf135
Owen Yi <owenyi333@gmail.com>
2023-11-25 10:40:01 +1100

    Finished translating text_wrap.py into test.el
    I realised that in line 80 of test.el, our method of determining whether to add text or add whitespace is rendered overcomplicated. Because `word_reduced` is always going to be (1 1 1 ... 1) up to length of the word. This also happens in the Python code though we didn't notice. Originally i think it was meant to be (1 1 1 ... 1 0) so there was some use to it. Anyway the xor comparision then boils down to how many of `look_ahead` is 1's, in which case we can just do the original (/ (apply '+ look_ahead) (length look_ahead)). We'll keep this for now in case we do need the extra complexity but otherwise we should simplify the code.
    
    TODO: pull out the necessary functions out of test.el into its own self contained progn script

--- elisp_test/test.el
+++ elisp_test/test.el
@@ -65,3 +65,62 @@
     (setq wrapped_line (concat wrapped_line " "))
     (setq pixel_count (1+ pixel_count))
     )
+
+
+(defun wrap-line (row text word_count)
+  (setq pixel_count 0)
+  (setq wrapped_line "")
+
+  (while (< pixel_count (length row))
+    (setq word (concat (nth word_count text) " "))
+    (setq word_len (length word))
+    
+    (setq look_ahead (pad-list-to-length (cl-subseq row pixel_count (min (length row) (+ pixel_count word_len))) word_len))
+    
+    (setq word_reduced (mapcar (lambda (x) 1) (split-string word "" t)))
+
+    (if (> (percent-similarity word_reduced look_ahead) .55)
+    (progn
+      (setq wrapped_line (concat wrapped_line word))
+      (setq word_count (1+ word_count))
+      (setq pixel_count (+ pixel_count word_len))
+      )
+    (setq wrapped_line (concat wrapped_line " "))
+    (setq pixel_count (1+ pixel_count))
+    )
+
+    
+    )
+
+  (list wrapped_line word_count)
+  )
+
+
+
+(defun create_wrapped_text (frame text)
+  (setq word_count 0)
+  (setq full_wrapped_text "")
+  (setq height (length frame))
+
+  (setq i 0)
+
+  (while (< i height)
+    (setq row (nth i frame))
+    (setq wrap-line-output (wrap-line row text word_count))
+
+    (setq wrapped_line (nth 0 wrap-line-output))
+    (setq word_count (nth 1 wrap-line-output))
+
+    ;(print (mapconcat 'number-to-string row ""))
+    ;(print wrapped_line)
+
+    (setq full_wrapped_text (concat full_wrapped_text wrapped_line "\n"))
+    
+    (setq i (1+ i))
+    )
+  
+  (switch-to-buffer "*scratch*")
+  (erase-buffer)
+  (goto-char (point-min))
+  (insert full_wrapped_text)
+  )
