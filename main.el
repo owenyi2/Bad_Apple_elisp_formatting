4e626497e9f8ac7972fdf62584cb936ee522c79e
Owen Yi <owenyi333@gmail.com>
2023-11-25 22:19:36 +1100

    aight all the stuff works now more or less. last commit for today

--- main.el
+++ main.el
@@ -1,49 +1,42 @@
 (require 'cl-lib)
 
-(defun read-file (file)
-  "read file by name FILE and return content as string"
-  (with-temp-buffer
-    (insert-file-contents file)
-    (goto-char (point-min))
-    (buffer-string)
-    )
-  )
-
 (defun pad-list-to-length (list length)
   "given LIST pad 0's to right until correct LENGTH."
   (append list (make-list (max 0 (- length (length list))) 0))
   )
 
 (defun wrap-line (row text_list word_count)
+  "given a 1D list ROW of 1's and 0's denoting the black white pixels in a particular row of a particular frame, and a 1D list TEXT_LIST of strings to use as words to render the frame, and an index WORD_COUNT denoting the words in TEXT_LIST used, produce a string of words from TEXT_LIST starting from WORD_COUNT to render ROW"
   (setq pixel_count 0)
   (setq wrapped_line "")
 
   (while (< pixel_count (length row))
     (setq word (concat (nth word_count text_list) " "))
     (setq word_len (length word))
-    
-    (setq look_ahead (pad-list-to-length (cl-subseq row pixel_count (min (length row) (+ pixel_count word_len))) word_len))
-    
-    (setq word_reduced (mapcar (lambda (x) 1) (split-string word "" t)))
+
+    "; grab the next WORD_LEN pixels from ROW so to determine if there's enough white pixel to add the next WORD"
+    (setq look_ahead (pad-list-to-length (cl-subseq row pixel_count (min (length row) (+ pixel_count word_len))) word_len)) 
 
     (if (> (/(apply '+ look_ahead) (length look_ahead)) .55)
     (progn
-      (setq wrapped_line (concat wrapped_line word))
+      (setq wrapped_line (concat wrapped_line word)) "; add WORD"
       (setq word_count (1+ word_count))
       (setq pixel_count (+ pixel_count word_len))
       )
-    (setq wrapped_line (concat wrapped_line " "))
+    (setq wrapped_line (concat wrapped_line " ")) "; add ' '"
     (setq pixel_count (1+ pixel_count))
     )
 
     
     )
-
+  "; need to pass on WORD_COUNT for next call of WRAP-LINE for the next ROW to keep track of position within TEXT_LIST"
   (list wrapped_line word_count)
+
   )
 
 
 (defun create_wrapped_text (frame text_list)
+  "call wrap-line a for each row in FRAME until we render entire FRAME with TEXT_LIST"
   (setq word_count 0)
   (setq full_wrapped_text "")
   (setq height (length frame))
@@ -62,32 +55,38 @@
     (setq i (1+ i))
     )
 
+  "; if we haven't used up all of TEXT_LIST shove the excess at the end"
   (if (< word_count (length text_list))
       (setq full_wrapped_text (concat full_wrapped_text "\n" (mapconcat 'identity (cl-subseq text_list word_count (length text_list)) " ")))
-      
-    
      
       )
   
   full_wrapped_text
   )
 
+(defun lisp--match-hidden-arg (limit) nil) "; suppress the red syntax highlighting produced by LISP--MATCH-HIDDEN-ARG flagging our crime-against-humanity of a lisp formatter"
+
 (defun main ()
-  (setq folder_path "/Users/owenyi/Desktop/Code Projects/Python/Bad_Apple/Autoformatter/")
   (setq frame_number 0)
+  (setq code_buffer (buffer-name))
 
+  (switch-to-buffer code_buffer)
+  (setq text_list (split-string (buffer-substring-no-properties (point-min) (point-max))))
+  
   (switch-to-buffer "*scratch*")
-  (font-lock-mode 0)
-  (text-scale-set -5)
+  (text-scale-set -3)
 
-  (switch-to-buffer "*temporary*")
+  (switch-to-buffer "*temporary*") "; use *temporary* to house the ./lisp_list.txt"
+  (buffer-disable-undo) "; avoid Warning (undo). we're not going to undo this buffer anyway so this is fine"
   (erase-buffer)
-  (insert-file-contents (concat folder_path "lisp_list.txt"))
+  (insert-file-contents "./lisp_list.txt") ";(change directory to where this file is first in emacs with M-x cd)"
   (goto-char (point-min))
 
- 
+  (print "BAD APPLE ON EMACS!!")
 
-  (while (not (eobp))
+  (while (not (eobp)) "; while not eobp in *temporary*"
+    (setq frame_start_time (float-time)) "; need to time how long rendering takes in order to adjust sleep time"
+	 
     (setq line_string (thing-at-point 'line))
     (setq line_string_stripped (cl-subseq line_string 0 (- (length line_string) 1)))
     (setq bad_apple_frame (eval (car (read-from-string line_string_stripped)))) 
@@ -102,18 +101,25 @@
     (insert wrapped_text)
     (goto-char (point-min))
 
-    (sit-for (/ 1 29.97))
+    (setq frame_number (1+ frame_number))
+    
+    (setq frame_time (- (float-time) frame_start_time))
+    (sit-for (max 0 (- (/ 1 29.97) frame_time)))
     
     (switch-to-buffer "*temporary*")
-    (setq frame_number (1+ frame_number))
-    (forward-line 1)
+    "; iterate line-by-line in *temporary* to flip through each bad apple frame"
+    (forward-line 1) 
 
       
     )
-  (switch-to-buffer "main.el")
+  (switch-to-buffer code_buffer)
   )
 
+"; my code is so delicate that even a touch sends the code into a freeze so I need to wrap it in a while-no-input for safety"
+(while-no-input (main)) "; <- ENTRY POINT: move cursor here and M-x eval-buffer"
 
-(main)
+
+";; Note that comments are encapsulated in quotes to allow breaking across lines"
+";; Our script is too short to cover the entire frame so I'm going to go spam the above function defintions below"
 
 
